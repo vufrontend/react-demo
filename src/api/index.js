b75a54ae2eb37254f344ed6541ee9092c2037060
vuluu <vu.luu@filestring.com>
2018-05-17 18:12:26 +0700

    update layout

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,14 @@
 const API_SERVER = 'https://api.giphy.com';
+const WEB_SERVER = 'https://giphy.com/api';
 const API_KEY = 'XNNemKvbx821jjL7rEwQNqp2c8PBoAJQ';
 
 export const END_POINT = {
     getTrendingGifs: {
         url: () => `${API_SERVER}/v1/gifs/trending?api_key=${API_KEY}&limit=20`,
+        method: 'GET'
+    },
+    getViewCount: {
+        url: (gifId) => `${WEB_SERVER}/v1/proxy-gif/${gifId}/view-count/`,
         method: 'GET'
     }
 }
--- src/components/listGif/GifCard.js
+++ src/components/listGif/GifCard.js
@@ -9,7 +9,7 @@ const propTypes = {
 class GifCard extends Component {
     render() {
         const data = this.props;
-        // console.log(data.data.images.preview_webp)
+        console.log(data.data)
         if(!data){
             return null;
         }
@@ -17,8 +17,20 @@ class GifCard extends Component {
         return (
             <div className="gif-block-wrapper col-lg-3 col-md-4 col-6">
                 <div className="gif-block">
-                    <div className="image-block">{!!images.preview_webp && <img src={images.original.url} alt='gif-trending'/>}</div>
-                    <div>{data.data.username}</div>
+                    <div className="image-block">
+                        {!!images.preview_webp && <img src={images.original.url} alt='trending gif'/>}
+                    </div>
+                    <div className="info-group-wrapper">
+                        <ul className="info-group">
+                            <li><i className="i-eye"></i>123</li>
+                            <li><i className="i-comment"></i>123</li>
+                            <li><i className="i-heart-o"></i>123</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="user-info-block">
+                    <div className="user-icon">{!!data.data.user && <img src={data.data.user.avatar_url} alt='gif-trending'/>}</div>
+                    <p className="user-name">{data.data.user.username}</p>
                 </div>
             </div>
         );
